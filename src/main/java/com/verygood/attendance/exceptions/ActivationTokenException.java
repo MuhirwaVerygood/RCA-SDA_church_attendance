7ad57bfca31d6ef342a3fc328f39efb7772f60df
MuhirwaVerygood <verygoodmuhirwa22@gmail.com>
2024-09-30 03:04:43 -0400

    Addition of exceptions

---
+++ src/main/java/com/verygood/attendance/exceptions/ActivationTokenException.java
@@ -0,0 +1,9 @@
+package com.verygood.attendance.exceptions;
+
+
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
